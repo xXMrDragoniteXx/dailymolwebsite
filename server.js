b84ca4934fb894fef98884165f4888ddabf06281
xXMrDragoniteXx <43099964+xXMrDragoniteXx@users.noreply.github.com>
2018-09-21 19:20:27 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/gallary.html')
+})
+
+app.use(express.static(__dirname));
+
+// Simple in-memory store
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+]
+
+app.get("/dreams", (request, response) => {
+  response.send(dreams)
+})
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", (request, response) => {
+  dreams.push(request.query.dream)
+  response.sendStatus(200)
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
